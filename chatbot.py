e58376370264447c5c12e7b5f6c4297c010a2761
sajibasil <160210722+sajibasil@users.noreply.github.com>
2025-11-20 12:03:59 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,15 @@
+
+def customLLMBot(user_input, history):
+    global messages_prmt
+
+    messages_prmt.append({"role": "user", "content": user_input})
+
+    response = client.chat.completions.create(
+        messages=messages_prmt,
+        model="llama-3.3-70b-versatile",
+    )
+    print(response)
+    LLM_reply = response.choices[0].message.content
+    messages_prmt.append({"role": "assistant", "content": LLM_reply})
+
+    return LLM_reply
